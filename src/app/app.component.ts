514d39d451131a56b61a1568d7484c32081a339b
gabriel-remon <remongabi16@gmail.com>
2024-04-19 17:05:30 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ejercicio1';
+  edad1 = "";
+  edad2 = "";
+  suma=0;
+  promedio=0;
+
+  calcular(){
+    this.suma = parseInt(this.edad1) + parseInt(this.edad2);
+    this.promedio= this.suma/2;
+   
+  }
+  limpiar(){
+ this.suma = 0;
+ this.promedio=0
+ this.edad1= ""
+ this.edad2=""
+  }
+
 }
